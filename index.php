46393f371d80f4664d6589f83487da4d7050585d
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-09-30 15:02:56 +0100

    Displaying full description

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
                         }
                       }
                     ?>
-                    <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
+                 <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
                 </div>
                 
                 <button class="btn btn-outline-warning m-1" type="button"  data-toggle="collapse" data-target="#Rule<?php echo $result_rule['id'];?>" aria-expanded="false" aria-controls="collapseExample">
@@ -95,26 +95,28 @@
                 </button>
                 <div class="collapse" id="<?php echo 'Rule' . $result_rule['id'];?>">
                   <div class="card-body">
-                  <?php echo $result_rule['description']; ?>
-                    <table class="table table-striped" style= "width:100%; color: white; background-color: #333333">
-                      <thead class="thead-dark">
-                        <tr>
-                          <th scope="col">Resource</th>
-                          <th scope="col">Status</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                          <?php
-                            while($row = $result->fetch_assoc()) {
-                              echo '
-                              <tr>
-                              <td>'.$row["resource_name"].'</td>
-                              <td><div class="active-status">Compliant</div></td>
-                              </tr>';
-                            }
+                    <?php echo $result_rule['description']; ?>
+                  </div>
+                  <div class="card-body">
+                      <table class="table table-striped" style= "width:100%; color: white; background-color: #333333">
+                        <thead class="thead-dark">
+                          <tr>
+                            <th scope="col">Resource</th>
+                            <th scope="col">Status</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                              while($row = $result->fetch_assoc()) {
+                                echo '
+                                <tr>
+                                <td>'.$row["resource_name"].'</td>
+                                <td><div class="active-status">Compliant</div></td>
+                                </tr>';
+                              }
                           ?>
-                      </tbody>
-                    </table>
+                        </tbody>
+                      </table>
                   </div>
                   <button type="button" id="<?php echo 'Rule' . $result_rule['id'];?>" class="btn btn-outline-warning float-right m-1" data-toggle="modal" data-target="#newExcModal">Add Exception</button>
                   <button type="button" class="btn btn-outline-warning float-right m-1" data-toggle="modal" data-target="#historyModal">View Exception History</button>
