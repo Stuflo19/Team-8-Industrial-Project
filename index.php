6614616a10d674bc452d7e15b6c666047f968dd5
Stuflo19 <szflorence@dundee.ac.uk>
2022-09-30 14:34:37 +0100

    added missing ;

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   $compliant = mysqli_query($conn, $sql);
   while (($row = mysqli_fetch_array($compliant, MYSQLI_ASSOC)) != false){
     $non_compliant_ids[] = $row['resource_id'];
-    $non_compliant_rules[] = $row[''] // add the row in to the results (data) array
+    $non_compliant_rules[] = $row['']; 
   }
 ?>
 
