4b3e9b4524e9ca7d9f1dd0030c1a0852ce5f3965
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-04 16:53:42 +0100

    changing login page to index

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 <html lang="en" dir="ltr">
 
 <head>
-  <meta charset="utf-8">
-  <!-- page title -->
-  <title> Prototype | Home </title>
+
+<meta charset="utf-8">
+
 
   <!-- import viewport properties -->
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -29,218 +29,33 @@
 
   </header>
 
-  <!-- navigation bar with links -->
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="collapse navbar-collapse d-flex justify-content-around" id="navbarNav">
-      <ul class="mb-auto pl-0">
-        <li>Username: Customer Name</li>
-        <li>Role: Customer Role</li>
-      </ul>
-      <br>
-      <h1 class=m-auto> Company Name </h1>
-      <h2><i class='fa fa-refresh p-2'></i>Last checked: date</h2>
-    </div>
-  </nav>
 
-  <main class="container-fluid p-5">
+  <main>
 
-    <div class="row">
-      <div class="col-lg">
-        <div class="col-lg-7"> 
-          <h3>Compliance Rules</h3>
-        </div>
-          <!-- Complaince Rule and Status -->
-          <?php 
-            while($result_rule=mysqli_fetch_array($query))
-            {
-          ?>
-          <div class = "row mb-2"> 
-            <div class="col-lg">
-              <!-- Compliance Rule Card -->
-              <div class="card cardColor text-center m-auto">
-                <div class="card-body m-1 p-1">
-                  <p class="card-text pb-1 m-auto"> <?php echo $result_rule["name"];?> </p>
-                  <?php 
-                      $status ="active-status"; // compliant
-                      $status_text ="Compliant";
-                      foreach($compliant as $result_non_compl)
-                      {
-                        if ($result_rule['id'] == $result_non_compl['rule_id'])
-                        {
-                          $status ="exception-status";
-                          $status_text ="Non-Compliant";
-                          break;
-                        }
-                      }
-                    ?>
-                    <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
-                  </div>
-                  
-                  <button class="btn btn-outline-warning m-1" type="button"  data-toggle="collapse" data-target="#Rule<?php echo $result_rule['id'];?>" aria-expanded="false" aria-controls="collapseExample">
-                    View details
-                  </button>
-                  <div class="collapse" id="<?php echo 'Rule' . $result_rule['id'];?>">
-                    <div class="card-body">
-                    <table class="table table-striped" style="color:white">
-                      <thead class="thead-dark">
-                        <tr>
-                          <th scope="col">Resource</th>
-                          <th scope="col">Status</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                          <?php
-                            foreach($result as $row) {
-                              $checked = false;
-                              echo '
-                              <tr>
-                              <td style="text-align: left">'.$row["resource_name"].'</td>';
-                              
-                              if(in_array($row["id"], $non_compliant_ids))
-                              {
-                                foreach(array_keys($non_compliant_ids, $row['id']) as $index) {
-                                  $non_compliant_rules[$index] == $result_rule["id"] ? $checked = true : $checked = false;
-                                  if($checked) {break;}
-                                };
-                              }
+  <img src="brightSolid.jpg" alt="BrightSolid logo">
 
-                              //if the resource edxists in the id array && ruleID at index of resource in the rules array
-                              if($checked)
-                              {
-                                echo '<td style="vertical-align: middle"><div class="exception-status"> Non-Compliant</div></td>';
-                              }
-                              else
-                              {
-                                echo '<td style="vertical-align: middle"><div class="active-status">Compliant</div></td>';
-                              }
-                              echo '</tr>';
-                            }
-                            
-                          ?>
-                      </tbody>
-                    </table>
-                    </div>
-                    <button type="button" id="<?php echo 'Rule' . $result_rule['id'];?>" class="btn btn-outline-warning float-right m-1" data-toggle="modal" data-target="#newExcModal">Add Exception</button>
-                    <button type="button" class="btn btn-outline-warning float-right m-1" data-toggle="modal" data-target="#historyModal">View Exception History</button>
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-          <?php } ?>
-      </div>
 
-      <!-- Placeholder for pie chart when we get it working -->
-      <div class="col-lg-5" position="absolute">
-        <h3>Overall Compliance</h3>
-        <p> The objective is to get a pie chart display in here similar to the interface on our university attendance tracker "SEATs", which visualises a percentage of how many rules are compliant, those that have exceptions and those that are non-compliant
-          <!-- Doughnut Chart -->
-          <div>
-            <canvas id="myChart"></canvas>
-          </div>
-          
-        </div>
+  <form>
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+    <input type="username" id="loginuser" class="form-control" />
+    <label class="form-label" for="loginuser">Username</label>
+  </div>
 
-    </div>
-    <!-- Add exception Modal -->
-    <div class="modal fade" id="newExcModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content bg-dark" style="background-color: #115e67">
-          <div class="modal-header">
-            <h3 class="modal-title" id="exampleModalLabel">Add Exception</h3>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="form-group">
-                <label for="resources-list" class="col-form-label">Select a cloud resource:</label>
-                <select style= "width:100%; color: white; background-color: #333333" id="resources-list">
-                  <!-- Temp until we can read in resources from the db -->
-                  <option label="T1"></option>
-                  <option label="T2"></option>
-                  <option label="T3"></option>
-                  <option label="T4"></option>
-                </select>
-              </div>
-              <div class="form-group">
-                <label for="message-text" class="col-form-label">Justification:</label>
-                <textarea class="form-control" id="message-text" style="color: white; background-color: #333333"></textarea>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
-            <button type="button" class="btn btn-primary">Submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <!-- Password input -->
+  <div class="form-outline mb-4">
+    <input type="password" id="loginpassword" class="form-control" />
+    <label class="form-label" for="loginpassword">Password</label>
+  </div>
 
-    <!-- View History Modal -->
-    <div class="modal fade bd-example-modal-lg" id="historyModal" tabindex="-1" role="dialog" aria-labelledby="historyModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content bg-dark" style="background-color: #115e67">
-          <div class="modal-header">
-            <h3 class="modal-title" id="historyModalLabel">Exception History</h3>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-          <table class="table table-striped" style="color:white">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">Exception ID</th>
-                <th scope="col">Created By</th>
-                <th scope="col">Justification</th>
-                <th scope="col">Review Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mr Crabbs</td>
-                <td>This is a reason to check if it expands fully</td>
-                <td>24/06/2026</td>
-              </tr>
-            </tbody>
-          </table>
-              
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Back</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
+  <!-- Submit button -->
+  <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+</form>
   </main>
 
   <!-- Footer -->
-  <footer class="container-fluid page-footer footerDesign">
-    <div class="row">
-      <div class="col-lg-4">
-
-        <!-- Store description -->
-        <h6 class="mt-3">Footer Heading 1</h6>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet leo nunc. Aliquam augue nulla, ullamcorper in fringilla eget, pulvinar id tellus. Vestibulum eros tortor, porttitor a tortor sit amet, consectetur auctor nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut lacinia sagittis sapien id cursus.</p>
-      </div>
-
-    </div>
-
-    <div class="row">
-
-
-      <div class="col font-italic text-justify mt-2">
-
-        <p>This web app was developed by Stuart Florence, Neil McGuigan, Laura Naslenaite, Gregor Davis and Euan Storrie. </p>
-
-      </div>
-
-    </div>
-
+  <footer>
+   
   </footer>
   <!-- import bootstrap JS, Popper.js, and jQuery  -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
