9a59adada38d7671b451d58d615c51d57e877b39
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-04 16:42:31 +0100

    readding styles

--- login.php
+++ login.php
@@ -7,7 +7,19 @@
 <html lang="en" dir="ltr">
 
 <head>
- 
+
+<meta charset="utf-8">
+
+
+  <!-- import viewport properties -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- import bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'> 
+  <!-- import css file -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="scripts.js"></script>
+  <link rel="stylesheet" href="master.css">
 </head>
 
 
