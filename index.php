b8b9daab798733aca3e5b5398ab5b598fd5d5b21
Neil <nzmcguigan@dundee.ac.uk>
2022-10-05 14:42:18 +0100

    Maybe this time?

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
   <main class="container-fluid p-5">
 
-    <div class="row">
+    <div class="row" style="max-height:500px">
       
       <!-- Placeholder for pie chart when we get it working -->
       <div class="col-lg-5 chart">
