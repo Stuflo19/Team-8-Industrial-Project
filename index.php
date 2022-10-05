9e73abd453d88924557aca95fa6bfa0edd3bd36e
Neil <nzmcguigan@dundee.ac.uk>
2022-10-05 16:18:19 +0100

    added margin

--- index.php
+++ index.php
@@ -49,8 +49,7 @@
       <!-- Placeholder for pie chart when we get it working -->
       <div class="col-lg-6 chart">
         <h3>Overall Compliance</h3>
-        <p> The objective is to get a pie chart display in here similar to the interface on our university attendance tracker "SEATs", which visualises a percentage of how many rules are compliant, those that have exceptions and those that are non-compliant
-          <!-- Doughnut Chart -->
+        <!-- Doughnut Chart -->
         <div>
           <canvas id="myChart"></canvas>
         </div>
