90a2348de6d5496d1251d140d9a5718055b99d6f
Neil <nzmcguigan@dundee.ac.uk>
2022-10-05 14:21:20 +0100

    Fixing height

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
   <main class="container-fluid p-5">
 
-    <div class="row" style="max-height:150px">
+    <div class="row" style="max-height:750px">
       
       <!-- Placeholder for pie chart when we get it working -->
       <div class="col-lg-5 chart">
