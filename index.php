6e06bd1c37db6dd16ec4199a368dd90dd9a34d26
Neil <nzmcguigan@dundee.ac.uk>
2022-10-06 13:44:05 +0100

    Test

--- index.php
+++ index.php
@@ -59,7 +59,8 @@
       <!-- Review Dates -->
       <div class="col-lg-5 d-flex align-items-center p-4" >
       <table class="table fixed_header" style="color:white">
-        <thead style="position: sticky;top: 0" class="thead-dark">
+        <thead style="position: sticky; padding:0px;" class="thead-dark">
+          <tr>
             <th class="stickyHead" scope="col-lg">Resource
             </th>
             <th class="stickyHead" scope="col-lg">Rule ID
@@ -70,6 +71,7 @@
             </th>
             <th class="stickyHead" scope="col-lg">Review date
             </th>
+          </tr>
         </thead>
         <tbody>
           <tr>
