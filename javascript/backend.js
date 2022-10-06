9381449a35d6e4f2b24822dd0d3ba47610b5fba4
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-06 22:56:22 +0100

    Removing function

--- javascript/backend.js
+++ javascript/backend.js
@@ -62,58 +62,58 @@ function addOption(name, id){
   return option;
 }
 
-function addException(rule_rescourceType, rows_resource, rows_non_compliant,rows_except){
-  // var rows_resource = <?php echo json_encode($resource); ?>;
-  // var rows_non_compliant = <?php echo json_encode($non_compliant); ?>;
-  // var rows_except = <?php echo json_encode($exception); ?>;
-  //console.log(rule_rescourceType);
-  var ruleID = rule_rescourceType.split(",")[0];
-  //var resourceTypeID = rule_rescourceType.split(",")[1];
-  var resource_name = "";
-  var resource_id = 0;
-  var non_compl = 1;
-  //console.log(ruleID);
-  //console.log(resourceTypeID);
-  var select_dropdown = document.querySelector('#resourceList');
-  while (select_dropdown.firstChild) 
-  {
-    select_dropdown.removeChild(select_dropdown.firstChild);
-  }
+// function addException(rule_rescourceType, rows_resource, rows_non_compliant,rows_except){
+//   // var rows_resource = <?php echo json_encode($resource); ?>;
+//   // var rows_non_compliant = <?php echo json_encode($non_compliant); ?>;
+//   // var rows_except = <?php echo json_encode($exception); ?>;
+//   //console.log(rule_rescourceType);
+//   var ruleID = rule_rescourceType.split(",")[0];
+//   //var resourceTypeID = rule_rescourceType.split(",")[1];
+//   var resource_name = "";
+//   var resource_id = 0;
+//   var non_compl = 1;
+//   //console.log(ruleID);
+//   //console.log(resourceTypeID);
+//   var select_dropdown = document.querySelector('#resourceList');
+//   while (select_dropdown.firstChild) 
+//   {
+//     select_dropdown.removeChild(select_dropdown.firstChild);
+//   }
 
-  for(var i = 0; i < rows_non_compliant.length; i++) {
-    //looking if a rule has non-compliant resources
-    if(rows_non_compliant[i]['rule_id'] == ruleID)
-    {
-      //finding the name of a resource
-      for(var j = 0; j < rows_resource.length; j++)
-      {
-        if(rows_resource[j]['id'] == rows_non_compliant[i]['resource_id'])
-        {
-          //checking if a non-compliant resource has an axception -> making a resource compliant
-          for(var k=0; k<rows_except.length; k++)
-          {
-            if(rows_resource[j]['resource_name'] === rows_except[k]['exception_value'])
-            {
-              non_compl=0;
-              break;
-            }
-          } 
+//   for(var i = 0; i < rows_non_compliant.length; i++) {
+//     //looking if a rule has non-compliant resources
+//     if(rows_non_compliant[i]['rule_id'] == ruleID)
+//     {
+//       //finding the name of a resource
+//       for(var j = 0; j < rows_resource.length; j++)
+//       {
+//         if(rows_resource[j]['id'] == rows_non_compliant[i]['resource_id'])
+//         {
+//           //checking if a non-compliant resource has an axception -> making a resource compliant
+//           for(var k=0; k<rows_except.length; k++)
+//           {
+//             if(rows_resource[j]['resource_name'] === rows_except[k]['exception_value'])
+//             {
+//               non_compl=0;
+//               break;
+//             }
+//           } 
 
-          if(non_compl==1)
-          {
-            resource_name = rows_resource[j]['resource_name'];
-            resource_id = rows_resource[j]['id'];
-            console.log(resource_name, ruleID);
+//           if(non_compl==1)
+//           {
+//             resource_name = rows_resource[j]['resource_name'];
+//             resource_id = rows_resource[j]['id'];
+//             console.log(resource_name, ruleID);
 
-            var resourceID_ruleID = resource_id+"_"+ruleID;
-            select_dropdown.appendChild(addOption(resource_name, resourceID_ruleID));
-          }
+//             var resourceID_ruleID = resource_id+"_"+ruleID;
+//             select_dropdown.appendChild(addOption(resource_name, resourceID_ruleID));
+//           }
           
-          break;
-        }
-      }
-    }
+//           break;
+//         }
+//       }
+//     }
 
-  }
+//   }
 
-}
+// }
