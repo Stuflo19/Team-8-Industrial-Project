3487e36c80d152ed2d4cbd622f85f73f2e8252bb
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-07 16:17:59 +0100

    Restoring to previous ver 2

--- index.php
+++ index.php
@@ -207,7 +207,7 @@
               //ruleID
               $justif = $_POST['newJustification'];
               //echo $justif;
-              $IDs = $_POST['resourceList'];  // get resource ID and rule
+              $IDs = $_POST['resourceList'];
               $ruleID = intval(explode("_",$IDs)[1]);
               //exception_value
               $resourceID= intval(explode("_",$IDs)[0]);
@@ -224,7 +224,6 @@
            
               $addExceptionS="INSERT INTO exception(id, customer_id, rule_id,last_updated_by, exception_value, justification, review_date, last_updated, suspended) VALUES (".$len_exception. ", 1,". $ruleID .",'system','" . $exception_value . "','".$justif."', '" . $_POST['newReviewDate'] . "','". $date ."',0 );";
               $insertQ = mysqli_query($conn,$addExceptionS);
-              //$conn->close();
               // if(!$insertQ){
               //   echo("Error description: " . mysqli_error($con));
               // }
