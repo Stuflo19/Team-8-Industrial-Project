cf6bebcb14271fcf472b3a82317a6f8edd3e12ec
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-07 13:47:01 +0100

    Adding close conn

--- index.php
+++ index.php
@@ -224,6 +224,7 @@
            
               $addExceptionS="INSERT INTO exception(id, customer_id, rule_id,last_updated_by, exception_value, justification, review_date, last_updated, suspended) VALUES (".$len_exception. ", 1,". $ruleID .",'system','" . $exception_value . "','".$justif."', '" . $_POST['newReviewDate'] . "','". $date ."',0 );";
               $insertQ = mysqli_query($conn,$addExceptionS);
+              $conn->close();
               // if(!$insertQ){
               //   echo("Error description: " . mysqli_error($con));
               // }
