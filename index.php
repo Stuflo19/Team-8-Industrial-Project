84f0c123f1c8145081234811a894e850c1943dde
Neil <nzmcguigan@dundee.ac.uk>
2022-10-07 15:57:58 +0100

    Missed an element on styling

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
     </div>
       
       <div class="row">
-        <div class="col-lg">
+        <div class="col-lg text-center mt-4">
         
           <!-- Complaince Rule and Status -->
           <?php 
@@ -256,7 +256,8 @@
         </div>
       </div>
     </div>
-  </body>
+  </main>
+
 
   <!-- Footer -->
   <footer class="container-fluid page-footer footerDesign">
@@ -287,4 +288,4 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   
-
+</body>
