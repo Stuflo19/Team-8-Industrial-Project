dc988fe62f473351a6e44df1ec28588f3670f0f7
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-07 09:35:31 +0100

    Adding php to a new file

---
+++ PHP/addException.php
@@ -0,0 +1,31 @@
+<?php
+    include 'dbconnect.php';
+
+    $len_exception = count($exception)+1;
+    //last_updates = today's day
+    $date = date("Y-m-d H:i:sa");
+    //ruleID
+    $justif = $_POST['newJustification'];
+    //echo $justif;
+    $IDs = $_POST['resourceList'];
+    $ruleID = intval(explode("_",$IDs)[1]);
+    //exception_value
+    $resourceID= intval(explode("_",$IDs)[0]);
+    $exception_value = "";
+    while($row =mysqli_fetch_array($result_res))
+    {
+        if($row['id'] == $resourceID)
+        {
+            $exception_value = $row['resource_name'];
+        }
+    }
+
+    //customer_id & last_updated_by are FIXED values
+
+    $addExceptionS="INSERT INTO exception(id, customer_id, rule_id,last_updated_by, exception_value, justification, review_date, last_updated, suspended) VALUES (".$len_exception. ", 1,". $ruleID .",'system','" . $exception_value . "','".$justif."', '" . $_POST['newReviewDate'] . "','". $date ."',0 );";
+    console.log($addException);
+    $insertQ = mysqli_query($conn,$addExceptionS);
+
+    $conn->close();
+    header("../index.php")
+?>
--- index.php
+++ index.php
@@ -176,7 +176,7 @@
             </button>
           </div>
           <div class="modal-body">
-            <form action="" method="post"> 
+            <form action="addException.php" method="post"> 
               <div class="form-group">
                 <label for="resourceList" class="col-form-label">Select a cloud resource:</label>
                 <select style= "width:100%; color: white; background-color: #333333" name="resourceList" id="resourceList">
@@ -200,35 +200,33 @@
                   <button type="submit" class="btn btn-primary">Submit</button>
               </div>   
             </form>
-            <?php
-              //id
-              $len_exception = count($exception)+1;
-              //last_updates = today's day
-              $date = date("Y-m-d H:i:sa");
-              //ruleID
-              $justif = $_POST['newJustification'];
-              //echo $justif;
-              $IDs = $_POST['resourceList'];
-              $ruleID = intval(explode("_",$IDs)[1]);
-              //exception_value
-              $resourceID= intval(explode("_",$IDs)[0]);
-              $exception_value = "";
-              while($row =mysqli_fetch_array($result_res))
-              {
-                if($row['id'] == $resourceID)
-                {
-                  $exception_value = $row['resource_name'];
-                }
-              }
+              <!-- //id
+              // $len_exception = count($exception)+1;
+              // //last_updates = today's day
+              // $date = date("Y-m-d H:i:sa");
+              // //ruleID
+              // $justif = $_POST['newJustification'];
+              // //echo $justif;
+              // $IDs = $_POST['resourceList'];
+              // $ruleID = intval(explode("_",$IDs)[1]);
+              // //exception_value
+              // $resourceID= intval(explode("_",$IDs)[0]);
+              // $exception_value = "";
+              // while($row =mysqli_fetch_array($result_res))
+              // {
+              //   if($row['id'] == $resourceID)
+              //   {
+              //     $exception_value = $row['resource_name'];
+              //   }
+              // }
 
-              //customer_id & last_updated_by are FIXED values
+              // //customer_id & last_updated_by are FIXED values
            
-              $addExceptionS="INSERT INTO exception(id, customer_id, rule_id,last_updated_by, exception_value, justification, review_date, last_updated, suspended) VALUES (".$len_exception. ", 1,". $ruleID .",'system','" . $exception_value . "','".$justif."', '" . $_POST['newReviewDate'] . "','". $date ."',0 );";
-              echo $addException;
-              $insertQ = mysqli_query($conn,$addExceptionS);
+              // $addExceptionS="INSERT INTO exception(id, customer_id, rule_id,last_updated_by, exception_value, justification, review_date, last_updated, suspended) VALUES (".$len_exception. ", 1,". $ruleID .",'system','" . $exception_value . "','".$justif."', '" . $_POST['newReviewDate'] . "','". $date ."',0 );";
+              // console $addException;
+              // $insertQ = mysqli_query($conn,$addExceptionS);
+ -->
 
-
-             ?> 
           </div>
         </div>
       </div>
