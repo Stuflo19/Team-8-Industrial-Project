001bf9d9376ac7b4244c62560a4361f78beacddb
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-08 23:54:24 +0100

    Removing mode and headers in index.php

--- index.php
+++ index.php
@@ -424,14 +424,14 @@ form.addEventListener('click', function(event){
 async function postData(formattedFormData){
     const response = await fetch('PHP/addException.php',{
         method: 'POST',
-        mode: 'cors',
+        //mode: 'cors',
         //headers: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"},
         body: formattedFormData
     });
     const data = await response.text();
     //This should now print out the values that we sent to the backend-side
     console.log(data);
-    location.reload();
+    //location.reload();
 }
 
 function sendForm()
