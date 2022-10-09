4107835f9e0537b522a9ec3840000c8fba383295
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-09 20:25:44 +0100

    Fixing issues 1

--- PHP/readdb.php
+++ PHP/readdb.php
@@ -9,22 +9,24 @@
     $resource[] = $e;
   }
 
-  $sql = "SELECT * FROM resource WHERE account_id = 1";
   $result_res = mysqli_query($conn, $sql);
 
-  $sql = "SELECT * FROM resource WHERE account_id = 1";
   $result = mysqli_query($conn, $sql);
   while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != false)
   {
     $resources[] = $row;
   }
 
-
-
   /*========================================== 
   READING ALL FROM EXCEPTION WHERE CUSTOMER_ID
   ============================================*/
   $sql = "SELECT * FROM exception WHERE customer_id = 1";
+  $exceptions = mysqli_query($conn, $sql);
+  foreach($exceptions as $e)
+  {
+    $exception1[] = $e;
+  }
+
   $exceptions = mysqli_query($conn, $sql);
   while (($row = mysqli_fetch_array($exceptions, MYSQLI_ASSOC)) != false)
   {
@@ -42,7 +44,6 @@
     $non_compliance[] = $row;
   }
 
-  $sql = "SELECT * FROM non_compliance";
   $compliant = mysqli_query($conn, $sql);
   foreach($compliant as $e)
   {
@@ -57,12 +58,14 @@
     $rules[] = $row;
   }
 
-    // Adding to JS vars resource, non_compliance, exception
-    echo '<Script>
-    var resource = '. json_encode($resources) .'
-    var non_compliance = '. json_encode($non_compliance) .'
-    var exception = '. json_encode($exception) .'
-    var rules = '. json_encode($rules) .'
-    </Script>';
+  $query = mysqli_query($conn,"SELECT * FROM rule");
+
+  // Adding to JS vars resource, non_compliance, exception
+  echo '<Script>
+          var resource = '. json_encode($resources) .'
+          var non_compliance = '. json_encode($non_compliance) .'
+          var exception = '. json_encode($exception) .'
+          var rules = '. json_encode($rules) .'
+        </Script>';
   //$conn->close();
 ?>
