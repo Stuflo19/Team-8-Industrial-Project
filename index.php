a50f7039978470b8ddd81e8d7b685df79b8a7047
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-10 21:19:52 +0100

    fix

--- index.php
+++ index.php
@@ -137,8 +137,8 @@
                       }
                     ?>
                     <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
-                    <div class="<?php echo $num_comp;?>"> <?php echo Compliant Resources: $num_comp;?></div>
-                    <div class="<?php echo $num_non_comp;?>"> <?php echo Non-Compliant Resources: $num_non_comp;?></div>
+                    <div class="<?php echo $num_comp;?>"> <?php echo "Compliant Resources:" $num_comp;?></div>
+                    <div class="<?php echo $num_non_comp;?>"> <?php echo "Non-Compliant Resources:" $num_non_comp;?></div>
                   </div>
                 </div>
                   
