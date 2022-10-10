ded2ef6d56cac75864d48b85b465bfcbf8879c58
LauraNaslenaite <lnaslenaite@dundee.ac.uk>
2022-10-10 15:13:37 +0100

    Removing update in addexception

--- PHP/addException.php
+++ PHP/addException.php
@@ -19,16 +19,16 @@
     $exception_value =  $IDs[2];
     $add_update = $IDs[3];
 
-    if($add_update == 1)
-    {
+    // if($add_update == 1)
+    // {
         $addExceptionS="INSERT INTO exception(id, customer_id, rule_id,last_updated_by, exception_value, justification, review_date, last_updated, suspended) VALUES (".$len_exception. ", 1,". $ruleID .",'system','" . $exception_value . "','".$justif."', '" . $_POST['newReviewDate'] . "','". $date ."',0 );";
         $insertQ = mysqli_query($conn,$addExceptionS);
-    }
-    else
-    {
-        $updateException = "UPDATE exception SET justification ='".$justif."', review_date='".$_POST['newReviewDate']."', last_updated='".$date."' WHERE  exception_value = '".$exception_value."'";
-        $insertQ = mysqli_query($conn,$updateException);
-    }
+    // }
+    // else
+    // {
+    //     $updateException = "UPDATE exception SET justification ='".$justif."', review_date='".$_POST['newReviewDate']."', last_updated='".$date."' WHERE  exception_value = '".$exception_value."'";
+    //     $insertQ = mysqli_query($conn,$updateException);
+    // }
     
 
     }
