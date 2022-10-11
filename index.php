ae4ed3dd79977e51b380fa824d430b28bc4478e6
Gregor-Davis <55397523+Gregor-Davis@users.noreply.github.com>
2022-10-11 11:36:51 +0100

    bugfixing

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
   ob_start();
   include 'PHP/dbconnect.php';
   include 'PHP/readdb.php';
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 ?>
 
 <!DOCTYPE html>
