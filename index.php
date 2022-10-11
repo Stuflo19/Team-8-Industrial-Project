600aa2978dafc20715f638e7ed9c3ceab499e17c
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-12 00:15:06 +0100

    test

--- index.php
+++ index.php
@@ -152,6 +152,8 @@
                       }
                     ?>
                     <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
+                    <div><span id="<?php echo 'comp_notification' . $result_rule['id']?>" > </span></div>
+                    <div><span id="<?php echo 'non_comp_notification' . $result_rule['id']?>" > </span></div>
                   </div>
                   
                   <div>
