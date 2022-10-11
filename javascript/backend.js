36145fafd85976fc26842f6c5bf3383cd9fbf6c8
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-11 15:10:03 +0100

    fix

--- javascript/backend.js
+++ javascript/backend.js
@@ -151,7 +151,7 @@ async function generateResources() {
     }
   }
 
-  document.getElementById('notificaion' + result_rule.id).innerHTML = "counter";
+  document.getElementById('notification' + result_rule.id).innerHTML = "counter";
 }
 // https://www.javascripttutorial.net/javascript-dom/javascript-appendchild/#:~:text=The%20appendChild()%20is%20a,of%20a%20specified%20parent%20node.&text=In%20this%20method%2C%20the%20childNode,()%20returns%20the%20appended%20child.
 function addOption(name, id){
