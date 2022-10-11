96dbf1c548058c9ba8e2556353f6208ec27ca48b
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-11 14:00:03 +0100

    undo

--- index.php
+++ index.php
@@ -103,52 +103,48 @@
               <!-- Compliance Rule Card -->
             
               <div class="card cardColor text-center m-auto">
-              <div class="card-body m-1 p-1 d-flex justify-content-between">
-        
-                  <p>Rule: <?php echo $result_rule["id"];?></p>
-                  <div>
+                <div class="d-flex justify-content-between">
+                  <div class="card-body m-1 p-1">
                     <p class="card-text pb-1 m-auto"> <?php echo $result_rule["name"];?> </p>
                     <?php 
                       $status ="active-status"; // compliant
                       $status_text ="Compliant";
-
-
-                      $non_comp_total =0;
-                      $non_comp_except =0;
-
-
                       foreach($compliant as $result_non_compl)
                       {
                         if ($result_rule['id'] == $result_non_compl['rule_id'])
                         {
-                            $quer = "SELECT * FROM resource WHERE id=".$result_non_compl['resource_id'];
-                            $quer1 = mysqli_query($conn, $quer);
-                            $quer2 = mysqli_fetch_array($quer1);
+                          $status ="exception-status";
+                          $status_text ="Non-Compliant";
 
-                            $quer = "SELECT * FROM exception WHERE exception_value='".$quer2['resource_ref']."'";
-                            $quer1 = mysqli_query($conn, $quer);
-                            $quer2 = mysqli_fetch_array($quer1);
+                          $num_non_comp =0;
+                          $num_comp = 0;
+                         
 
-                            if($quer2== NULL || $quer2['suspended'] == 1)
+                          foreach($compliant as $result_non_compl)
+                          {
+                            if ($result_rule['id'] == $result_non_compl['rule_id'])
                             {
-                              $non_comp_total =  $non_comp_total +1;
-                              if($quer2['suspended'] == 1)
-                              {
-                                $non_comp_except = $non_comp_except+1;
-                              }
-                              $status ="exception-status";
-                              $status_text ="Non-Compliant";
-                              break;
+                        
+      
+                                if($quer2== NULL || $quer2['suspended'] == 1)
+                                {
+                                  $num_non_comp =  $num_non_comp +1;
+                            
+                                
+                                }
                             }
-=======
-        
+                          }
+                          break;
                         }
                       }
                     ?>
                     <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
-                    </div>
-                  <span class="badge">69</span>
-                    </div>
+                    <div id="<?php echo $num_comp;?>" class = "compliance_counter"> <?php echo "Compliant Resources: " . $display_comp;?></div>
+                    <div id="<?php echo $display_non_comp;?>" class = "compliance_counter"> <?php echo "Non-Compliant Resources: " . $display_non_comp;?></div>
+                    <?php  $display_non_comp =0;  $display_comp =0; ?>
+                  </div>
+                </div>
+                  
                 <button class="btn btn-outline-warning m-1" type="button"  data-toggle="collapse" data-target="#Rule<?php echo $result_rule['id'];?>" aria-expanded="false" aria-controls="collapseExample">
                   View details
                 </button>
