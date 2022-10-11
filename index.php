820c2496c9aa6c5307c6bf07d7889767d863ca17
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-12 00:57:43 +0100

    undo

--- index.php
+++ index.php
@@ -152,14 +152,12 @@
                       }
                     ?>
                     <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
-           
+                    <div><p class = "resource-counter" id="<?php echo 'comp_notification' . $result_rule['id'];?>" > </p></div>
+                    <div><p class = "resource-counter" id="<?php echo 'non_comp_notification' . $result_rule['id'];?>" > </p></div>
                   </div>
                   
                   <div>
                     <span class="badge">69</span>
-                    <div class = "resource-counter"
-                    <p id="<?php echo 'comp_notification' . $result_rule['id'];?>" > </p>
-                    <p id="<?php echo 'non_comp_notification' . $result_rule['id'];?>" > </p>
                   </div>
 
                 </div>
