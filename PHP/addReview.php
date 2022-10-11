d5285f3c26e28698a546ea0909753144b3326396
Neil <nzmcguigan@dundee.ac.uk>
2022-10-11 17:35:17 +0100

    Changed the value of insert stmt

--- PHP/addReview.php
+++ PHP/addReview.php
@@ -10,6 +10,6 @@
     $oldReview = $_POST['oldReview'];
     $date = date('d-m-y h:i:s');
 
-    $sql = "INSERT INTO `exception_audit`(`exception_id`, `user_id`, `customer_id`, `rule_id`, `action`, `action_dt`, `old_exception_value`, `new_exception_value`, `old_justification`, `new_justification`, `old_review_date`, `new_review_date`) VALUES (".$newJustification.",'1','1',".$ruleId.",'Review',".$date.",".$exceptionValue.",".$exceptionValue.",".$oldJustification.",".$newJustification.",".$oldReview.",".$newReview.")";
+    $sql = "INSERT INTO `exception_audit`(`exception_id`, `user_id`, `customer_id`, `rule_id`, `action`, `action_dt`, `old_exception_value`, `new_exception_value`, `old_justification`, `new_justification`, `old_review_date`, `new_review_date`) VALUES (".$exceptionId.",'1','1',".$ruleId.",'Review',".$date.",".$exceptionValue.",".$exceptionValue.",".$oldJustification.",".$newJustification.",".$oldReview.",".$newReview.")";
     mysqli_query($conn, $sql);
 ?>
