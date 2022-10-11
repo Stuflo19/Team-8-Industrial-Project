44662ec3c78e68be248393c5d75b61ab71041755
Neil <nzmcguigan@dundee.ac.uk>
2022-10-11 14:26:05 +0100

    Set id on rule card body

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
               <!-- Compliance Rule Card -->
               <div class="card cardColor text-center m-auto">
                 
-                <div class="card-body m-1 p-1 d-flex justify-content-between">
+                <div class="card-body m-1 p-1 d-flex justify-content-between" id="<?php echo 'RuleCard' . $result_rule['id'];?>">
                   <p>Rule: <?php echo $result_rule["id"];?></p>
                   <div>
                     <p class="card-text pb-1 m-auto"> <?php echo $result_rule["name"];?> </p>
@@ -303,7 +303,8 @@
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
+  <script src="js/zenscroll-min.js"></script>
+
 </body>
 <?php
     $_POST = array();
