f008a7b50b123c2bcd94aa54c191031428958d14
Neil <nzmcguigan@dundee.ac.uk>
2022-10-11 17:23:40 +0100

    Added echo for sql

--- PHP/addReview.php
+++ PHP/addReview.php
@@ -11,5 +11,7 @@
     $date = date('d-m-y h:i:s');
 
     $sql = "INSERT INTO exception_audit(exception_id, 'user_id', customer_id, rule_id, 'action', action_dt, old_exception_value, new_exception_value, old_justification, new_justification, old_review_date, new_review_date) VALUES (".$newJustification.",'1','1',".$ruleId.",'Review',".$date.",".$exceptionValue.",".$exceptionValue.",".$oldJustification.",".$newJustification.",".$oldReview.",".$newReview.")";
+    echo $sql;
     mysqli_query($conn, $sql);
+
 ?>
