dbd3307e18685e974618207cfbab64aa46c4f002
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-12 00:36:58 +0100

    fix

--- index.php
+++ index.php
@@ -152,8 +152,8 @@
                       }
                     ?>
                     <div class="<?php echo $status;?>"> <?php echo $status_text;?></div>
-                    <div><p class = "resource-counter" id="<?php echo 'comp_notification' . $result_rule['id'];?>" >Compliant Resources: </p></div>
-                    <div><p class = "resource-counter" id="<?php echo 'non_comp_notification' . $result_rule['id'];?>" >Non-Compliant Resources: </p></div>
+                    <div><p class = "resource-counter" id="<?php echo 'comp_notification' . $result_rule['id'];?>" > </p></div>
+                    <div><p class = "resource-counter" id="<?php echo 'non_comp_notification' . $result_rule['id'];?>" > </p></div>
                   </div>
                   
                   <div>
