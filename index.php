0dd3bdcd27578439670921d09d08d1a2ba84dc8a
euanstorrie <55396404+euanstorrie@users.noreply.github.com>
2022-10-12 15:56:00 +0100

    test

--- index.php
+++ index.php
@@ -157,10 +157,11 @@
                   
              
                     <span class="badge">69</span>
-
+                    
+                    <div>
                     <p class = "resource-counter" id="<?php echo 'comp_notification' . $result_rule['id'];?>" > </p>
                     <p id="<?php echo 'non_comp_notification' . $result_rule['id'];?>" style = "text-align: right; position: relative; top: -70px;"> </p>
-               
+                    </div>
 
                 </div>
                   
