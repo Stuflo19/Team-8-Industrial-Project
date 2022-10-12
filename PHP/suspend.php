7bf70a33f2991a3e5e8e4a95cc340ecaef6e1d16
Stuflo19 <szflorence@dundee.ac.uk>
2022-10-12 14:06:10 +0100

    Added session to suspend

--- PHP/suspend.php
+++ PHP/suspend.php
@@ -1,9 +1,13 @@
 <?php
+    session_start();
     include 'dbconnect.php';
 
     if(isset($_POST['id']) && isset($_POST['suspended'])){
         $id = $_POST['id'];
         $suspended = $_POST['suspended'];
+        $customer_id = $_SESSION['customer'];
+        $user_id = $_SESSION['user_id'];
+        $date = date("Y-m-d H:i:sa");
         
         $suspended == 1 ? $suspended = 0: $suspended = 1;
         $suspended == 1 ? $action="Suspended" : $action = "Unsuspended";
