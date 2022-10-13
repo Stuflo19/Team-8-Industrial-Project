f4b7811cb7938ff35d005ecfc1545d60f41aa0a6
Neil <nzmcguigan@dundee.ac.uk>
2022-10-13 18:28:35 +0100

    Trying without compliance audit stuff

--- PHP/removeException.php
+++ PHP/removeException.php
@@ -16,39 +16,20 @@ if(isset($_POST['newJustification1']))
     $customer_id = $_SESSION['customer'];
     $user_id = $_SESSION['user_id'];
 
-    echo '--------';
-    $sqlFetchID = "SELECT resource_id FROM resource WHERE resource_ref=".$exceptionValue."";
-    echo $sqlFetchID;
-    $result1 = mysqli_query($conn, $sqlFetchID);
-    echo '--------'. $result1;
-    $resourceID = mysqli_fetch_array($result1);
-
-    echo 'Resource ID: ' .$resourceID. '';
-
-    $sqlFetchID = "SELECT rule_id FROM `non_compliance` WHERE resource_id=".$resourceID."";
-    $result2 = mysqli_query($conn, $sqlFetchID);
-    $non_compliance_id = mysqli_fetch_array($result2);
-
-    echo 'Compliance ID: ' .$non_compliance_id. '';
-
-    
-
-    echo $customer_id;
-
-    $sql = "INSERT INTO `exception_audit`(`exception_id`, `user_id`, `customer_id`, `rule_id`, `action`, `action_dt`, `old_exception_value`, `old_justification`, `new_justification`, `old_review_date`) VALUES ('$exceptionId','$user_id','$customer_id','$ruleId','Delete','$date', '$exceptionValue', '$oldJustification', '$newJustification', '$oldReview')";
+    $sql = "DELETE FROM `exception` WHERE id = $exceptionId";
     mysqli_query($conn, $sql);
+
+    echo "Deleting Exception";
+
+    $sql1 = "INSERT INTO `exception_audit`(`exception_id`, `user_id`, `customer_id`, `rule_id`, `action`, `action_dt`, `old_exception_value`, `old_justification`, `new_justification`, `old_review_date`) VALUES ('$exceptionId','$user_id','$customer_id','$ruleId','Delete','$date', '$exceptionValue', '$oldJustification', '$newJustification', '$oldReview')";
+    mysqli_query($conn, $sql1);
 
     echo "Inserting Exception Audit";
     
-    $sql1 = "INSERT INTO `non_compliance_audit`(`non_compliance_id`, `resource_id`, `rule_id`, `user_id`, `action`, `action_dt`) VALUES ('".$non_compliance_id."', '".$resourceID."', '".$ruleId."', '$user_id', 'Delete', '".$date."') ";   
-    mysqli_query($conn, $sql1);
+    // $sql2 = "INSERT INTO `non_compliance_audit`(`non_compliance_id`, `resource_id`, `rule_id`, `user_id`, `action`, `action_dt`) VALUES ('".$non_compliance_id."', '".$resourceID."', '".$ruleId."', '$user_id', 'Delete', '".$date."') ";   
+    // mysqli_query($conn, $sql2);
 
-    echo "Inserting Compliance Audit";
-
-    $sql2 = "DELETE FROM `exception` WHERE id = $exceptionId";
-    mysqli_query($conn, $sql2);
-
-    echo "Deleting Exception";
+    // echo "Inserting Compliance Audit";
     
     $conn->close();
 }
